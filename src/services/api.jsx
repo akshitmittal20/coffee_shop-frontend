069c2c0b5ff17d8bad8212f871776dfc71114613
unknown <mittal.akshit@cognitensor.com>
2024-06-23 15:43:50 +0530

    cicd

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend API URL
+  baseURL: 'http://44.202.166.106:5000/api', // Backend API URL
 });
 
 export default api;
